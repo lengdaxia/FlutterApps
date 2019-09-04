09fc45c55432a94f23fe84d2bbe55a5677c32cee
Marlon Leng <marlonleng@MarlondeiMac.local>
2019-09-04 12:03:25 +0900

    wip

--- Flutter_30_days/lists/lib/main.dart
+++ Flutter_30_days/lists/lib/main.dart
@@ -21,17 +21,36 @@ class ListExamplePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+
     var dividedWidgetList = ListTile.divideTiles(
       context: context,
       tiles: _getListData(),
       color: Colors.black
     ).toList();
 
+    var listView = ListView(children: dividedWidgetList);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('List Example'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              print('添加了');
+              
+              dividedWidgetList.add(
+                // Text('New Row')
+                Padding(
+                 padding: EdgeInsets.all(10.0),
+                  child: Text('Row new'),
+                   )
+              );
+            },
+          ),
+        ],
       ),
-      body: ListView(children: dividedWidgetList,),
+      body: listView,
     );
   }
 
